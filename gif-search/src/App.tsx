d98fbfeb075d963dc4a40f8a51927b6100a04f84
Nikita <nik200830@gmail.com>
2025-06-14 17:30:49 +0300

    dz123

---
+++ gif-search/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GifSearch from './components/GifSearch';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GifSearch />
+    </div>
+  );
+}
+
+export default App; 
